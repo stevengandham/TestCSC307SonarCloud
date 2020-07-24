57629003581e77821ce9d32f869c64108daf907f
stevengandham <stevengandham@gmail.com>
2020-07-23 22:18:00 -0700

    Fixed more smells

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,11 @@
+package world;
+
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		Logger L = Logger.getLogger(HelloWorld.class.getName());
-		L.log(Level.INFO, "Hello world!");
+		Logger logger = Logger.getLogger(HelloWorld.class.getName());
+		logger.log(Level.INFO, "Hello world!");
 	}
 }
