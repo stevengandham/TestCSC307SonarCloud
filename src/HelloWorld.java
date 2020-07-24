b10c4b304ce4a6f1a6239c1fb81245f2038a4db3
stevengandham <stevengandham@gmail.com>
2020-07-23 21:54:50 -0700

    Changed System.out to Logger.log

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,9 @@
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+		Logger L = Logger.getLogger(HelloWorld.class.getName());
+		L.log(Level.INFO, "Hello world!");
 	}
 }
